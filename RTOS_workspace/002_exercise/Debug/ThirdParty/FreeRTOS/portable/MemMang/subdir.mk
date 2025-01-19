1c20f7b836dfa123ac04ef377d156cb3a25a0910
thuyh0h <trongthuynl2004@gmail.com>
2025-01-19 20:37:48 +0700

    exercise: using button to tongle led v1(polling)

---
+++ RTOS_workspace/002_exercise/Debug/ThirdParty/FreeRTOS/portable/MemMang/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c 
+
+OBJS += \
+./ThirdParty/FreeRTOS/portable/MemMang/heap_4.o 
+
+C_DEPS += \
+./ThirdParty/FreeRTOS/portable/MemMang/heap_4.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ThirdParty/FreeRTOS/portable/MemMang/%.o ThirdParty/FreeRTOS/portable/MemMang/%.su ThirdParty/FreeRTOS/portable/MemMang/%.cyclo: ../ThirdParty/FreeRTOS/portable/MemMang/%.c ThirdParty/FreeRTOS/portable/MemMang/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/SEGGER/Config" -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/SEGGER/SEGGER" -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/FreeRTOS/include" -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/SEGGER/OS" -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/FreeRTOS" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-ThirdParty-2f-FreeRTOS-2f-portable-2f-MemMang
+
+clean-ThirdParty-2f-FreeRTOS-2f-portable-2f-MemMang:
+	-$(RM) ./ThirdParty/FreeRTOS/portable/MemMang/heap_4.cyclo ./ThirdParty/FreeRTOS/portable/MemMang/heap_4.d ./ThirdParty/FreeRTOS/portable/MemMang/heap_4.o ./ThirdParty/FreeRTOS/portable/MemMang/heap_4.su
+
+.PHONY: clean-ThirdParty-2f-FreeRTOS-2f-portable-2f-MemMang
+
---
+++ RTOS_workspace/002_exercise/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c 
+
+OBJS += \
+./ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o 
+
+C_DEPS += \
+./ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ThirdParty/SEGGER/SEGGER/Syscalls/%.o ThirdParty/SEGGER/SEGGER/Syscalls/%.su ThirdParty/SEGGER/SEGGER/Syscalls/%.cyclo: ../ThirdParty/SEGGER/SEGGER/Syscalls/%.c ThirdParty/SEGGER/SEGGER/Syscalls/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/SEGGER/Config" -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/SEGGER/SEGGER" -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/FreeRTOS/include" -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/SEGGER/OS" -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/002_exercise/ThirdParty/FreeRTOS" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-ThirdParty-2f-SEGGER-2f-SEGGER-2f-Syscalls
+
+clean-ThirdParty-2f-SEGGER-2f-SEGGER-2f-Syscalls:
+	-$(RM) ./ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.cyclo ./ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d ./ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o ./ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.su
+
+.PHONY: clean-ThirdParty-2f-SEGGER-2f-SEGGER-2f-Syscalls
+
