9f6b2e85d9ccebb4a01e56edb15b650b1735309f
thuyh0h <trongthuynl2004@gmail.com>
2025-01-23 14:57:02 +0700

    005_FreeRTOS_TaskPriority

---
+++ RTOS_workspace/005_FreeRTOS_TaskPriority/Debug/ThirdParty/FreeRTOS/event_groups.d
@@ -0,0 +1,41 @@
+ThirdParty/FreeRTOS/event_groups.o: ../ThirdParty/FreeRTOS/event_groups.c \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/Config/Global.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/task.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/list.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/timers.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/task.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/event_groups.h \
+ D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/timers.h
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/Config/Global.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/portable.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/task.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/list.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/timers.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/task.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/event_groups.h:
+D:/Microcontroller/STM32F40x/STM32F4_RTOS/RTOS_workspace/STM32F407DISC1_FreeRTOS_Project_Template/ThirdParty/FreeRTOS/include/timers.h:
